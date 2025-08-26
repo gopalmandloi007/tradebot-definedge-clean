c64749534c6615037b17e90c403f2a33b18f8323
gopalmandloi007 <gopalmandloi007@gmail.com>
2025-08-27 00:24:10 +0530

    Update __init__.py

--- tradebot/__init__.py
+++ tradebot/__init__.py
@@ -1,1 +1,14 @@
-# tradebot package
+"""
+TradeBot package initializer.
+Exposes high-level interfaces for session, orders, data, etc.
+"""
+
+from .session_manager import SessionManager
+from .order_manager import OrderManager
+from .holdings_manager import HoldingsManager
+from .positions_manager import PositionsManager
+from .symbol_manager import SymbolManager
+from .historical_data import HistoricalData
+from .data_manager import DataManager
+from .downloader import Downloader
+from .disaster_recovery import DisasterRecovery

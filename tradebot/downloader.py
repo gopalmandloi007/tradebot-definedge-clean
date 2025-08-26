3b47a21b2d6c71788dc1d75aa7fa5e51fc8a6361
gopalmandloi007 <gopalmandloi007@gmail.com>
2025-08-27 00:19:56 +0530

    Update downloader.py

--- tradebot/downloader.py
+++ tradebot/downloader.py
@@ -1,3 +1,17 @@
-# downloader: incremental history download logic (uses history_meta)
-def download_history_segment(segment, token, timeframe, from_ts, to_ts):
-    raise NotImplementedError
+import pandas as pd
+import os
+from datetime import datetime
+
+def save_to_csv(data, filename_prefix="data"):
+    """Save any list/dict data to CSV file."""
+    df = pd.DataFrame(data)
+    filename = f"{filename_prefix}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.csv"
+    df.to_csv(filename, index=False)
+    return filename
+
+def save_to_excel(data, filename_prefix="data"):
+    """Save data to Excel file."""
+    df = pd.DataFrame(data)
+    filename = f"{filename_prefix}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.xlsx"
+    df.to_excel(filename, index=False)
+    return filename
